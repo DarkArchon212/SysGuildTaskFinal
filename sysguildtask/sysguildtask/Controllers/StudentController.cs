2772ff24654b706c3ac7b3bb72948d8d19fe958e
Robert S <rob.searle.d@gmail.com>
2018-05-21 17:06:37 -0500

    Comments added to project
    Added comments to explain thinking behind code and areas i've gotten stuck

--- sysguildtask/sysguildtask/Controllers/StudentController.cs
+++ sysguildtask/sysguildtask/Controllers/StudentController.cs
@@ -20,6 +20,7 @@ namespace sysguildtask.Controllers
             _context = context;
             if (_context.Students.Count() == 0)
             {
+                //Add new student objects to context to be saved
                 _context.Students.Add(new Student { FirstName = "Robert", LastName = "Searle", EmailID = "rsearle@hotmail.com", PhoneNo = "224-456-9800" });
                 _context.Students.Add(new Student { FirstName = "Autumn", LastName = "Ellis",  EmailID = "aellis@gmail.com",    PhoneNo = "331-845-7459" });
                 _context.Students.Add(new Student { FirstName = "Anna",   LastName = "Searle", EmailID = "asearle@yahoo.com", PhoneNo = "224-326-1562" });
@@ -30,30 +31,18 @@ namespace sysguildtask.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            return View("Views/Index");
         }
 
 
         [HttpGet]
         public List<Student> GetAll()
         {
+            //reuturn list of student object from context db
+            //How do i get this list information into view to display in html table foreach loop
+            //Return list data seen as output
             return _context.Students.ToList();
         }
-
-        /*
-        [HttpGet("{FirstName}", Name = "GetStudent")]
-        public IActionResult GetByName(String name)
-        {
-            var stud = _context.Students.Find(name);
-            if(stud == null)
-            {
-                return NotFound();
-            }
-            return Ok(stud);
-        }
-        */
-        
-
 
     }//end of controller
 }
--- sysguildtask/sysguildtask/Models/StudentContext.cs
+++ sysguildtask/sysguildtask/Models/StudentContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+
+//Controller for Context for get and set methods for a student into db
 namespace sysguildtask.Models
 {
     public class StudentContext : DbContext
@@ -7,8 +9,9 @@ namespace sysguildtask.Models
         public StudentContext(DbContextOptions<StudentContext> options)
             : base(options)
         {
-        }
 
+        }
+        
         public DbSet<Student> Students { get; set; }
     }
 }
